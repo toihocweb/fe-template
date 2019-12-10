6e09a207a49f20d17b51d75b3a5aa9c32bcefb74
nhat.bui <nhat.bui@easycredit.vn>
2019-12-10 15:10:57 +0700

    #add fe-template

---
+++ README.md
@@ -0,0 +1,39 @@
+# FE Template
+
+[![N|Solid](https://i.imgur.com/BmorCRp.png)](https://www.facebook.com/Cheatsomething)
+
+FE template is a simple template for FE Developers,
+what i use..
+
+- HTML5
+- SCSS
+- JQUERY
+- GULP 4
+- FONT AWESOME 5
+- ....
+
+# New Features!
+
+> Do not wait; the time will never be 'just right.' Start where you stand
+> and work with whatever tools you may have at your command,
+> and better tools will be found as you go along.
+
+### Installation
+
+Install gulp-cli first (ignore it if you're already)
+
+```sh
+$ npm i gulp-cli
+```
+
+Install the dependencies and devDependencies and start the server.
+
+```sh
+$ cd fe-template
+$ npm i
+$ npm start
+```
+
+## License
+
+MIT
---
+++ gulpfile.js
@@ -0,0 +1,95 @@
+const gulp = require("gulp");
+const plumber = require("gulp-plumber");
+const rename = require("gulp-rename");
+const sass = require("gulp-sass");
+const autoPrefixer = require("gulp-autoprefixer");
+//if node version is lower than v.0.1.2
+require("es6-promise").polyfill();
+const cssComb = require("gulp-csscomb");
+const cmq = require("gulp-merge-media-queries");
+const cleanCss = require("gulp-clean-css");
+const uglify = require("gulp-uglify");
+  browserSync = require("browser-sync").create(),
+  reload = browserSync.reload;
+
+const imagemin = require("gulp-imagemin");
+
+gulp.task("scss", function() {
+  return gulp
+    .src(["src/scss/**/*.scss"])
+    .pipe(
+      plumber({
+        handleError: function(err) {
+          console.log(err);
+          this.emit("end");
+        }
+      })
+    )
+    .pipe(sass())
+    .pipe(autoPrefixer())
+    .pipe(cssComb())
+    .pipe(cmq({ log: true }))
+    .pipe(gulp.dest("dist/css"))
+    .pipe(
+      rename({
+        suffix: ".min"
+      })
+    )
+    .pipe(cleanCss())
+    .pipe(gulp.dest("dist/css"));
+});
+
+
+gulp.task("image", function() {
+  gulp
+    .src("src/img/*")
+    .pipe(imagemin())
+    .pipe(gulp.dest("dist/img"));
+});
+
+gulp.task("js", function() {
+  return gulp
+    .src(["src/js/**/*.js"])
+    .pipe(
+      plumber({
+        handleError: function(err) {
+          console.log(err);
+          this.emit("end");
+        }
+      })
+    )
+    .pipe(gulp.dest("dist/js"))
+    .pipe(
+      rename({
+        suffix: ".min"
+      })
+    )
+    .pipe(uglify())
+    .pipe(gulp.dest("dist/js"));
+});
+gulp.task("html", function() {
+  gulp
+    .src(["*.html"])
+    .pipe(
+      plumber({
+        handleError: function(err) {
+          console.log(err);
+          this.emit("end");
+        }
+      })
+    )
+    .pipe(gulp.dest("."));
+});
+gulp.task("serve", function() {
+  // Serve files from the root of this project
+  browserSync.init({
+    server: {
+      baseDir: "."
+    }
+  });
+
+  gulp.watch("src/js/**/*.js", gulp.series("js")).on("change", reload);
+  gulp.watch("src/scss/**/*.scss", gulp.series("scss")).on("change", reload);
+  gulp.watch("src/img/*", gulp.series("image")).on("change", reload);
+  gulp.watch("*.html", gulp.series("html")).on("change", reload);
+});
