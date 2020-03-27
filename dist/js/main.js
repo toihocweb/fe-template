edd5d497c1f0983ee9235daa2615cd9fd20cc860
toihocweb <buidinhnhat2031@gmail.com>
2020-03-27 20:51:16 +0700

    #add bt 5

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,54 +1,27 @@
-//-- kiểu dữ liệu trong js là kiểu dữ liệu động (nghĩa là 1 biến có thể là 1 string hoặc 1 number....)
-// number, string, object, boolen ...symbol
+// DOM (document object model)
+// goi selector
 
-// const , let , var
+// let content = document.getElementById("content").textContent;
+// let clickme = document.getElementById("clickme").textContent;
 
-// ctrl + s
+// getElementById
+// getElementsByClassName
 
-// 2 nguyen tac cuar const: 1. không đc khai báo lại, 2. không đc gán lại giá trị
+let btnSelectors = document.getElementsByClassName("clickme");
 
-// const number = 50;
-
-// let number = 10;
-
-// function trong js
-
-// cach 1
-function printName(msg) {
-  console.log(msg);
+for (let i = 0; i <= btnSelectors.length - 1; i++) {
+  var a = 0;
+  btnSelectors[i].addEventListener("click", () => {
+    for (let j = 0; j <= btnSelectors.length - 1; j++) {
+      btnSelectors[j].style.backgroundColor = "initial";
+    }
+    btnSelectors[i].style.backgroundColor = "red";
+  });
 }
 
-// cach 2 - function expression
-var printProductName = function(msg) {
-  console.log(msg);
-};
+// methods of array in js
+// splice, toString, push, pop , shift, unshift, concat, slice,
 
-//cach 3 - arrow function
-const printAddress = (addr, number) => {
-  console.log(`dia chi truong la ${number} ${addr}`);
-};
+// sort() in js
 
-// printAddress("Man thien", 97);
-
-// định nghĩa 1 arrow function có 1 tham số là (number) in ra kết quả
-// là 1 chẳn hay không
-
-// object se co key : value
-let user = {
-  name: "Nhat Bui",
-  age: 20
-};
-
-// định nghĩa 1 arrow function có 1 tham số (user) in ra age trong
-// user có lớn hơn 18+ hay không
-
-const checkAge = user => {
-  return user.age > 18
-    ? "co the xem phim 18+"
-    : "con nit khong nen xem phim 18+";
-};
-
-console.log(checkAge(user));
-// checkAge(user);
-
-// cai 1 so extension ho tro
+// iteration in js
