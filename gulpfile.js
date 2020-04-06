2ae3199c4e5d2aa3a8bf65f1bd2419297981faaa
toihocweb <buidinhnhat2031@gmail.com>
2020-04-06 15:27:10 +0700

    #fixed dev

--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,16 @@ const plumber = require("gulp-plumber");
 const rename = require("gulp-rename");
 const sass = require("gulp-sass");
 const autoPrefixer = require("gulp-autoprefixer");
-//if node version is lower than v.0.1.2
 require("es6-promise").polyfill();
 const cssComb = require("gulp-csscomb");
 const cmq = require("gulp-merge-media-queries");
 const cleanCss = require("gulp-clean-css");
-// const uglify = require("gulp-uglify");
 (browserSync = require("browser-sync").create()), (reload = browserSync.reload);
 const uglify = require("gulp-uglify-es").default;
 
 const imagemin = require("gulp-imagemin");
 
-gulp.task("scss", function() {
+gulp.task("scss", function () {
   return gulp
     .src(["src/scss/**/*.scss"])
     .pipe(sass())
@@ -24,49 +22,46 @@ gulp.task("scss", function() {
     .pipe(gulp.dest("dist/css"))
     .pipe(
       rename({
-        suffix: ".min"
+        suffix: ".min",
       })
     )
     .pipe(cleanCss())
     .pipe(gulp.dest("dist/css"));
 });
 
-gulp.task("image", function() {
-  return gulp
-    .src("src/img/*")
-    .pipe(imagemin())
-    .pipe(gulp.dest("dist/img"));
+gulp.task("image", function () {
+  return gulp.src("src/img/*").pipe(imagemin()).pipe(gulp.dest("dist/img"));
 });
 
-gulp.task("js", function() {
+gulp.task("js", function () {
   return gulp
     .src(["src/js/**/*.js"])
     .pipe(
       plumber({
-        handleError: function(err) {
+        handleError: function (err) {
           console.log(err);
           this.emit("end");
-        }
+        },
       })
     )
     .pipe(gulp.dest("dist/js"))
     .pipe(
       rename({
-        suffix: ".min"
+        suffix: ".min",
       })
     )
     .pipe(uglify())
     .pipe(gulp.dest("dist/js"));
 });
-gulp.task("html", function() {
+gulp.task("html", function () {
   return gulp.src(["*.html"]).pipe(gulp.dest("dist/"));
 });
-gulp.task("serve", function() {
+gulp.task("serve", function () {
   // Serve files from the root of this project
   browserSync.init({
     server: {
-      baseDir: "."
-    }
+      baseDir: ".",
+    },
   });
 
   gulp.watch("src/js/**/*.js", gulp.series("js")).on("change", reload);
