aef3649263f32ed27a8f5eb0a1d4eb2e3a5e78d5
toihocweb <buidinhnhat2031@gmail.com>
2020-05-20 14:58:43 +0700

    #ad

---
+++ src/js/main.js
@@ -0,0 +1,23 @@
+// types: kiểu dữ liệu (kiểu dữ liệu động - dynamic type)
+// primitive type: number , boolean, string, null, undefine,
+// reference type: object, array, function
+
+// const , let , var
+// === : so sanh gia tri va kieu du lieu
+// == : so sanh gia tri
+
+// kiểm tra 1 số chẳn lẽ
+// var number = 11;
+
+//  return 1 so ngau nhien 0->1 (khong bao gom so 1)
+console.log(Math.random());
+
+console.log(Math.floor(4.7)); //lam tron xuong
+console.log(Math.round(4.6)); //lam tron den so gan nhat
+console.log(Math.ceil(4.1)); // lam tron len
+
+// random 1 so tu nhien tu 0->10
+console.log(Math.ceil(Math.random() * 10));
+Math.floor(Math.random() * 11);
+
+Math.floor(Math.random() * 10 + 1);
