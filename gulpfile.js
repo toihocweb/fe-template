68a7d85f4030ab6cae2408ff864930837873da49
Toi Hoc Web <buidinhnhat2036@gmail.com>
2021-04-10 12:28:08 +0700

    new template

--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,27 @@
 const gulp = require("gulp");
-const plumber = require("gulp-plumber");
 const rename = require("gulp-rename");
 const sass = require("gulp-sass");
 const autoPrefixer = require("gulp-autoprefixer");
-require("es6-promise").polyfill();
 const cssComb = require("gulp-csscomb");
 const cmq = require("gulp-merge-media-queries");
 const cleanCss = require("gulp-clean-css");
 (browserSync = require("browser-sync").create()), (reload = browserSync.reload);
-const uglify = require("gulp-uglify-es").default;
-
-const imagemin = require("gulp-imagemin");
 
 gulp.task("scss", function () {
   return gulp
-    .src(["src/scss/**/*.scss"])
+    .src(["scss/**/*.scss"])
     .pipe(sass())
     .pipe(autoPrefixer())
     .pipe(cssComb())
     .pipe(cmq({ log: true }))
-    .pipe(gulp.dest("dist/css"))
+    .pipe(gulp.dest("./css"))
     .pipe(
       rename({
         suffix: ".min",
       })
     )
     .pipe(cleanCss())
-    .pipe(gulp.dest("dist/css"));
-});
-
-gulp.task("image", function () {
-  return gulp.src("src/img/*").pipe(imagemin()).pipe(gulp.dest("dist/img"));
-});
-
-gulp.task("js", function () {
-  return gulp
-    .src(["src/js/**/*.js"])
-    .pipe(gulp.dest("dist/js"))
-    .pipe(
-      rename({
-        suffix: ".min",
-      })
-    )
-    .pipe(uglify())
-    .pipe(gulp.dest("dist/js"));
+    .pipe(gulp.dest("./css"));
 });
 
 gulp.task("serve", function () {
@@ -54,8 +32,6 @@ gulp.task("serve", function () {
     },
   });
 
-  gulp.watch("src/js/**/*.js", gulp.series("js")).on("change", reload);
-  gulp.watch("src/scss/**/*.scss", gulp.series("scss")).on("change", reload);
-  gulp.watch("src/img/*", gulp.series("image")).on("change", reload);
+  gulp.watch("scss/**/*.scss", gulp.series("scss")).on("change", reload);
   gulp.watch("*.html").on("change", reload);
 });
